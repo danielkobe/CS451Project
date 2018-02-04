9a963c635015de5508fd9b50b4f22752b0ce656a
Daniel Koberstein <daniel.koberstein@wsu.edu>
2018-02-03 18:05:52 -0800

    started business GUI

---
+++ Milestone1/Milestone1/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+
+namespace Milestone1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Business
+        {
+            public Business (string name, string state)
+            {
+                this.name = name;
+                this.state = state;
+            }
+            public string name { get; set; }
+            public string state { get; set; }
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddStates();
+            AddColumnsToGrid();
+        }
+
+        private string BuildConnString()
+        {
+            return "Server=localhost; Database=Milestone1DB; Port=5433; Username=postgres; Password=Bix53z7h4m";
+        }
+
+        public void AddStates()
+        {
+            using (var conn = new NpgsqlConnection(BuildConnString()))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT DISTINCT state FROM business ORDER BY state;";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            StateList.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+
+                conn.Close();
+            }
+        }
+
+        public void AddColumnsToGrid()
+        {
+            DataGridTextColumn col1 = new DataGridTextColumn();
+            col1.Header = "Business Name";
+            col1.Binding = new Binding("name");
+            col1.Width = 255;
+            BusinessGrid.Columns.Add(col1);
+
+            DataGridTextColumn col2 = new DataGridTextColumn();
+            col2.Header = "State";
+            col2.Binding = new Binding("state");
+            col2.Width = 255;
+            BusinessGrid.Columns.Add(col2);
+        }
+
+        private void StateListSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BusinessGrid.Items.Clear();
+            using (var conn = new NpgsqlConnection(BuildConnString()))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT DISTINCT name,state FROM business WHERE state = '" + StateList.SelectedItem.ToString() + "';";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            BusinessGrid.Items.Add(new Business(reader.GetString(0), reader.GetString(1)));
+                        }
+                    }
+                }
+
+                conn.Close();
+            }
+
+        }
+    }
+}
