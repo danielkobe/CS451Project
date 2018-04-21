e92f2233bf59f1347523fe53e45cca4c86c7db4c
humberto <hmejia27@outlook.com>
2018-04-21 12:40:23 -0700

    fixing merge conflict

---
+++ Milestone-2/Milestone1/CheckInWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Milestone1
+{
+    /// <summary>
+    /// Interaction logic for CheckInWindow.xaml
+    /// </summary>
+    public partial class CheckInWindow : Window
+    {
+        public CheckInWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
